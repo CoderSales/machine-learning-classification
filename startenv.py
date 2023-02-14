be34e040f3a22ea46896d8abf9ec797e9cae996c
Coder731 <stephenobrien1@gmail.com>
2023-02-14 20:47:19 +0100

    Add from main3 py to starten py as pseudocode

---
+++ startenv.py
@@ -0,0 +1,34 @@
+import os
+
+# part z:
+# modules_list to pass modules
+modules_list = [
+    "notebook",
+    "matplotlib",
+    "pandas",
+    "seaborn",
+    "numpy",
+    "scipy",
+    "statsmodels",
+    "-U scikit-learn",
+    "ipykernel",
+    "nb-black",
+]
+
+
+# part a:
+# add checker
+non_installed=[]
+for package in packages:
+    
+    if
+    
+    # pass out non-installed elements
+    else:
+
+
+# part b:
+os.system(
+    ". .venv/Scripts/activate && pip install notebook && pip install matplotlib && pip install pandas && pip install seaborn && pip install numpy && pip install scipy && pip install statsmodels && pip install -U scikit-learn && pip install ipykernel && pip install nb-black"
+)
+
